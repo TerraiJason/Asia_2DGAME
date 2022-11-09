1948643b00b50af3ade8403db3cfd81c6ad2575f
TerraiJason <84991443+TerraiJason@users.noreply.github.com>
2022-11-09 20:41:58 +0800

    Update 開始寫程式

---
+++ UnityProject/My project/Assets/Scripts/Movesystem.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+namespace TerraiJason
+{
+   /// <summary>
+   /// 移動系統
+   /// </summary>
+   public class Movesystem : MonoBehaviour
+   {
+        [Range(-10,0), SerializeField, Header("移動速度")]
+        private float speed = -3.5f; 
+   }
+
+}
